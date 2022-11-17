be689dcc51b5a4fe129dafb1282ad587403dd07b
ubermensch101 <76596256+ubermensch101@users.noreply.github.com>
2022-11-17 19:12:06 +0530

    Add files via upload

---
+++ FUNCTIONS.py
@@ -0,0 +1,31 @@
+import random
+def check_user_exists(user_ID):
+    return True
+def check_user_password(user_ID, user_password):
+    return True
+def add_user(user_ID, user_password):
+    return
+def set_user_online(user_ID):
+    return
+def set_user_offline(user_ID):
+    return
+def send_text(sender_ID, receiver_ID, text_message):
+    return
+def send_image(sender_ID, receiver_ID, image):
+    return
+def create_group(group_ID, admin_ID):
+    return
+def check_group_exists(group_ID):
+    return random.randint(0, 1)
+def check_admin(group_Id, admin_ID):
+    return random.randint(0, 1)
+def add_member(group_ID, user_ID):
+    return
+def remove_member(group_ID, user_ID):
+    return
+def check_member(group_ID, user_ID):
+    return True
+def send_group_text(sender_ID, group_ID, text_message):
+    return
+def send_group_image(sender_ID, group_ID, image):
+    return
