34140d5cd1804c4f6336ec33d25b97b5bd0c1002
ubermensch101 <76596256+ubermensch101@users.noreply.github.com>
2022-11-26 00:44:20 +0530

    Final Commit

--- client/run_clients_terminal.sh
+++ client/run_clients_terminal.sh
@@ -6,4 +6,4 @@ cd database
 python3 initialise_database.py
 cd ../client
 echo "Close terminal when the process is completed"
-xterm -e "python3 main_client.py < test_clients/test_client.txt > test_clients/test_output.txt"
+xterm -e "python3 main_client.py < test_clients/test_client.txt"
